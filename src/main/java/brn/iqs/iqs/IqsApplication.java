7927bedbd8b9e385bc417199edbc40e2b42ec47d
Chris <chriskbrown@hotmail.co.uk>
2020-02-03 11:00:52 +0000

    Spring Init

---
+++ src/main/java/brn/iqs/iqs/IqsApplication.java
@@ -0,0 +1,13 @@
+package brn.iqs.iqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqsApplication.class, args);
+	}
+
+}
